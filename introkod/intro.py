406a5aaa0ad596aa917c784c90a3c9a59e3ee272
Antonsoder <anton.soderstrom@elev.ga.ntig.se>
2020-08-28 09:37:19 +0200

    introduktionskod

---
+++ introkod/intro.py
@@ -0,0 +1,14 @@
+print("hej TE19C")
+
+name = "Anton"
+
+
+
+print("ditt namn är" ,name)
+
+
+side = float(input("Ange kvadratens sida: "))
+
+area = side**2
+
+print("Arean av kvadraten är {area} a.e. ")
