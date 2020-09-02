d6fe8725bf8f0a0063f9eec0179df76081dbb293
Antonsoder <anton.soderstrom@elev.ga.ntig.se>
2020-09-02 13:23:19 +0200

    test

--- jdjdj.py
+++ jdjdj.py
@@ -1,4 +1,4 @@
-sida = input("ange kvadratens sida")
+sida = float(input("ange kvadratens sida"))
 omkrets = 4*sida
-area: sida**2
+area = sida**2
 print(f"omkretsen är{omkrets}l.e och area är {area}a.e")
